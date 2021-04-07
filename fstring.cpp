fbbec26044e3b597d49cdc78f149277b13af47ce
Qubits-ops <72653472+Qubits-ops@users.noreply.github.com>
2021-04-07 21:20:59 +0200

    Create fstring.cpp

---
+++ fstring.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+
+using namespace std;
+
+void fstring(vector<char> texte){
+    int compteur = 0;
+    while (texte.size() > 0){
+            cout << texte[compteur];
+            compteur++;
+            texte.pop_back();
+
+    }
+
+
+}
+
+int main()
+{
+    cout << "Hello World !" << endl;
+    vector<char> aff(0);
+    aff.push_back('s');
+    aff.push_back('a');
+    aff.push_back('l');
+    aff.push_back('u');
+    aff.push_back('t');
+    fstring(aff);
+}
